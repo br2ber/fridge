5c1ab6ba9ff3a776d2cd94e34c75cf838d0c6a2c
Brosset Bertrand <brossetb@proximity.bbdo.fr>
2016-08-12 15:56:58 +0200

    no message

--- app/models/degree.js
+++ app/models/degree.js
@@ -2,7 +2,7 @@ var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
 var degreeSchema   = new Schema({
-    name: String
+    temperature: String
 });
 
 module.exports = mongoose.model('Degree', degreeSchema);
