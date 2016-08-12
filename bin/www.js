81f3e8ec3e6d292a2459f1dd7a3c54d63ae44276
Brosset Bertrand <brossetb@proximity.bbdo.fr>
2016-08-12 15:36:26 +0200

    no message

--- bin/www.js
+++ bin/www.js
@@ -8,7 +8,7 @@ var express    = require('express');        // call express
 var app        = express();                 // define our app using express
 var bodyParser = require('body-parser');
 var mongoose   = require('mongoose');
-var Fridge     = require('../app/models/fridge');
+var Fridge     = require('../app/models/degree');
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.urlencoded({ extended: true }));
